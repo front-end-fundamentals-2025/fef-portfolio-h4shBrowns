81f0796e642893eb0efbcde224837200f0a3fcb7
h4shBrowns <fohe24kc@student.ju.se>
2025-02-21 16:16:58 +0100

    add rotation to main images

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const catImages = document.querySelector (".cat-images")
+const mainImages = document.querySelectorAll(".main-images");
 
-catImages.addEventListener("click", e => {
-    console.log("This is a silly cat")
-}) 
+mainImages.forEach((image) => {
+  image.addEventListener("mouseover", function () {
+    this.style.transform = "rotate(180deg)";
+  });
+
+  image.addEventListener("mouseout", function () {
+    this.style.transform = "rotate(0deg)"; 
+  });
+});
